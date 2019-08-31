f05afd59ad21390067bc36675b1d6aafe532902b
adrianmcg85 <adrian.m.mcg@gmail.com>
2019-08-31 21:54:48 +0100

    adjusting bootstrap references

--- features/reporting.php
+++ features/reporting.php
@@ -5,11 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <?php include 'bootstrap/style.css'?>
+    <?php include '../bootstrap/style.css'?>
 </head>
 <body>
     <h1>Bug Reporting</h1>
-
-    <?php include 'bootstrap/script.js'?>
+    <div class="container">
+    <div class="row">
+    <h3>Device Type</h3>
+    <div class="col">
+    <strong>mobile</strong>
+    <br>
+    <input type="radio" name="device" id="device_mobile">
+    </div>
+    </div>
+    </div>
+    <?php include '../bootstrap/script.js'?>
 </body>
 </html>
