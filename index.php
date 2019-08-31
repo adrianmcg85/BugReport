ba4a9457777365ea1891802adb1264e8bdb65c35
adrianmcg85 <adrian.m.mcg@gmail.com>
2019-08-31 18:46:30 +0100

    added bootstrap files in folder and included them to index.php

--- index.php
+++ index.php
@@ -5,10 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <?php include 'bootstrap/style.css'?>
 </head>
 <body>
     <h1>Bug Reporting and Analysis</h1>
     <a href="#">Report a Bug</a>
     <a href="#">Review Bugs found on your project</a>
+    <?php include 'bootstrap/script.js'?>
 </body>
 </html>
