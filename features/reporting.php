627f88982c1bf867bf4accf3f794295f7c05dce1
adrianmcg85 <adrian.m.mcg@gmail.com>
2019-08-31 22:24:16 +0100

    created a bug submission form and submit_bug file

--- features/reporting.php
+++ features/reporting.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,18 +8,49 @@
     <title>Document</title>
     <?php include '../bootstrap/style.css'?>
 </head>
+
 <body>
     <h1>Bug Reporting</h1>
     <div class="container">
-    <div class="row">
-    <h3>Device Type</h3>
-    <div class="col">
-    <strong>mobile</strong>
-    <br>
-    <input type="radio" name="device" id="device_mobile">
-    </div>
-    </div>
+        <form action="submit_bug.php" method="post">
+            <h3>Device Type</h3>
+            <div class="row">
+                <div class="col-1">
+                    <strong>Desktop</strong>
+                    <br>
+                    <input type="radio" name="device" id="device_desktop">
+                </div>
+                <div class="col-1">
+                    <strong>Tablet</strong>
+                    <br>
+                    <input type="radio" name="device" id="device_tablet">
+                </div>
+                <div class="col-1">
+                    <strong>mobile</strong>
+                    <br>
+                    <input type="radio" name="device" id="device_mobile">
+                </div>
+            </div>
+            <div class="row">
+                <h3>Browser</h3>
+                <br>
+                <div class="col">
+                    <select name="browser" id="browser">
+                        <option value="Chrome">Chrome</option>
+                        <option value="IE11">IE11</option>
+                        <option value="Firefox">Firefox</option>
+                    </select>
+                </div>
+            </div>
+            <div class="row">
+                <h3>Description</h3>
+                <br>
+                <textarea name="description" id="description" cols="30" rows="5"></textarea>
+            </div>
+            <input type="submit" value="Submit">
+        </form>
     </div>
     <?php include '../bootstrap/script.js'?>
 </body>
+
 </html>
