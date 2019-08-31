6a74d1d43130f2b6423e53eb38a10330e3aaa1cc
adrianmcg85 <adrian.m.mcg@gmail.com>
2019-08-31 21:48:07 +0100

    adding basic html with bootstrap and a h1 header

---
+++ features/reporting.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <?php include 'bootstrap/style.css'?>
+</head>
+<body>
+    <h1>Bug Reporting</h1>
+
+    <?php include 'bootstrap/script.js'?>
+</body>
+</html>
